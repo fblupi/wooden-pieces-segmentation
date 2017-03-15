7c3264b4d3577ca309390cee91f7980ba1a08261
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-15 11:24:50 +0100

    Canny Edge Detection Filter applied

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <itkImageFileReader.h>
 #include <itkImageToVTKImageFilter.h>
 #include <itkVTKImageToImageFilter.h>
-#include <itkBinaryThresholdImageFilter.h>
+#include <itkCastImageFilter.h>
+#include <itkCannyEdgeDetectionImageFilter.h>
+#include <itkRescaleIntensityImageFilter.h>
 
 #include <vtkSmartPointer.h>
 #include <vtkImageViewer2.h>
@@ -11,19 +13,46 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
+	float variance = 1;
+	float upperThreshold = 10;
+	float lowerThreshold = 0;
+
 	if (argc < 2) {
-		std::cerr << "Usage: " << std::endl;
-		std::cerr << argv[0] << " inputImageFile" << std::endl;
+		cerr << "Usage: " << endl;
+		cerr << argv[0] << " inputImageFile" << endl;
 		return EXIT_FAILURE;
 	}
 
-	typedef itk::Image<signed short, 2> ImageType;
-	typedef itk::ImageFileReader<ImageType> ReaderType;
-	typedef itk::ImageToVTKImageFilter<ImageType> ImageToVTKImageType;
-	typedef itk::VTKImageToImageFilter<ImageType> VTKImageToImageType;
-	typedef itk::BinaryThresholdImageFilter<ImageType, ImageType> FilterType;
+	if (argc > 2) {
+		variance = atof(argv[2]);
+	}
+
+	if (argc > 3) {
+		upperThreshold = atof(argv[3]);
+	}
+
+	if (argc > 4) {
+		lowerThreshold = atof(argv[4]);
+	}
+
+	const unsigned int Dimension = 2;
+	typedef signed short CharPixelType;
+	typedef float RealPixelType;
+
+	typedef itk::Image<CharPixelType, Dimension> CharImageType;
+	typedef itk::Image<RealPixelType, Dimension> RealImageType;
+
+	typedef itk::CastImageFilter<CharImageType, RealImageType> CastToRealFilterType;
+	typedef itk::CannyEdgeDetectionImageFilter<RealImageType, RealImageType> CannyFilterType;
+	typedef itk::RescaleIntensityImageFilter<RealImageType, CharImageType> RescaleFilterType;
+
+	typedef itk::ImageFileReader<CharImageType> ReaderType;
+	typedef itk::ImageToVTKImageFilter<CharImageType> ImageToVTKImageType;
+	typedef itk::VTKImageToImageFilter<CharImageType> VTKImageToImageType;
 
 	vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
 	reader->SetFileName(argv[1]);
@@ -33,28 +62,42 @@ int main(int argc, char *argv[])
 	connectorInput->SetInput(reader->GetOutput());
 	connectorInput->Update();
 
-	FilterType::Pointer filter = FilterType::New();
-	filter->SetInput(connectorInput->GetOutput());
-	filter->SetOutsideValue(150);
-	filter->SetInsideValue(180);
-	filter->SetLowerThreshold(0);
-	filter->SetUpperThreshold(255);
+	CastToRealFilterType::Pointer toReal = CastToRealFilterType::New();
+	CannyFilterType::Pointer filter = CannyFilterType::New();
+	RescaleFilterType::Pointer rescale = RescaleFilterType::New();
+	
+	toReal->SetInput(connectorInput->GetOutput());
+	filter->SetInput(toReal->GetOutput());
+	filter->SetVariance(variance);
+	filter->SetUpperThreshold(upperThreshold);
+	filter->SetLowerThreshold(lowerThreshold);
 	filter->Update();
+	rescale->SetInput(filter->GetOutput());
 
 	ImageToVTKImageType::Pointer connectorOutput = ImageToVTKImageType::New();
-	connectorOutput->SetInput(filter->GetOutput());
+	connectorOutput->SetInput(rescale->GetOutput());
 	connectorOutput->Update();
 
-	vtkSmartPointer<vtkImageViewer2> imageViewer = vtkSmartPointer<vtkImageViewer2>::New();
-	imageViewer->SetInputData(connectorOutput->GetOutput());
-	
-	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	imageViewer->SetupInteractor(renderWindowInteractor);
-	imageViewer->Render();
-	imageViewer->GetRenderer()->ResetCamera();
-	imageViewer->Render();
+	vtkSmartPointer<vtkImageViewer2> imageViewerInput = vtkSmartPointer<vtkImageViewer2>::New();
+	imageViewerInput->SetInputData(reader->GetOutput());
 
-	renderWindowInteractor->Start();
+	vtkSmartPointer<vtkImageViewer2> imageViewerOutput = vtkSmartPointer<vtkImageViewer2>::New();
+	imageViewerOutput->SetInputData(connectorOutput->GetOutput());
+
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractorInput = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	imageViewerInput->SetupInteractor(renderWindowInteractorInput);
+	imageViewerInput->Render();
+	imageViewerInput->GetRenderer()->ResetCamera();
+	imageViewerInput->Render();
+
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractorOutput = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	imageViewerOutput->SetupInteractor(renderWindowInteractorOutput);
+	imageViewerOutput->Render();
+	imageViewerOutput->GetRenderer()->ResetCamera();
+	imageViewerOutput->Render();
+
+	renderWindowInteractorOutput->Start();
+	renderWindowInteractorInput->Start();
 
 	return EXIT_SUCCESS;
 }
